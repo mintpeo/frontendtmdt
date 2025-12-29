a11c46a89ad311b2d37fac662b95c68e6314f6af
khai <khaipham103@gmail.com>
2025-12-29 19:00:36 +0700

    Merge main into khai branch and resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import './App.css'
-import Home from './home/home.jsx'
-import Navbar from './navbar/navbar.jsx'
-import Footer from './footer/footer.jsx'
+import "./App.css";
+import Home from "./home/home.jsx";
+import Navbar from "./navbar/navbar.jsx";
+import Footer from "./footer/footer.jsx";
 import React from "react";
 import Snowfall from "react-snowfall";
 
 function App() {
-
   return (
     <>
-        <Snowfall color="#82C3D9" style={{position: "fixed", zIndex: 9999}} />
-        <Navbar />
-        <main>
-            <Home />
-            {/* <Routes>...</Routes> */}
-        </main>
-        <Footer />
+      <Snowfall color="#82C3D9" style={{ position: "fixed", zIndex: 9999 }} />
+      <Navbar />
+      <main>
+        <Home />
+        {/* <Routes>...</Routes> */}
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
