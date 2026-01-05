f93bd429df88fd391d66a0e65b50d9168e11bf12
khai <khaipham103@gmail.com>
2026-01-05 15:35:18 +0700

    Format code: Update App.jsx formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Snowfall from "react-snowfall";
-import Scroll from './service/ScrollToTop.jsx'
-import './App.css'
+import Scroll from "./service/ScrollToTop.jsx";
+import "./App.css";
 
-import Home from './home/home.jsx'
-import Navbar from './navbar/navbar.jsx'
-import Footer from './footer/footer.jsx'
-import Login from './user/login/login.jsx'
-import Sign from './user/sign/sign.jsx'
-import Info from './user/infoUser/info.jsx'
+import Home from "./home/home.jsx";
+import Navbar from "./navbar/navbar.jsx";
+import Footer from "./footer/footer.jsx";
+import Login from "./user/login/login.jsx";
+import Sign from "./user/sign/sign.jsx";
+import Info from "./user/infoUser/info.jsx";
 
 function App() {
   return (
     <>
-        <Router>
-            <Scroll />
-            <Snowfall color="#82C3D9" style={{position: "fixed", zIndex: 9999}} />
-            <Navbar />
-            <main className="main-content">
-                <Routes>
-                    <Route path="/" element={<Home />}/>
-                    <Route path="/user/login" element={<Login />}/>
-                    <Route path="/user/sign" element={<Sign />}/>
-                    <Route path="/user/info" element={<Info />}/>
-                </Routes>
-            </main>
-            <Footer />
-        </Router>
+      <Router>
+        <Scroll />
+        <Snowfall color="#82C3D9" style={{ position: "fixed", zIndex: 9999 }} />
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user/login" element={<Login />} />
+            <Route path="/user/sign" element={<Sign />} />
+            <Route path="/user/info" element={<Info />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
